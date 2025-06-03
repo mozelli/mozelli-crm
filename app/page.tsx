804e48354d7550afbdf33ddc816d4779ad725ad8
João Mozelli Neto <joaomozelli@hotmail.com>
2025-06-03 15:38:13 -0300

    remove desnecessary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
 import Link from "next/link";
 
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
