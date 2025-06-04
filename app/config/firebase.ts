318dc81a78bb6b65198ec72f5202d48616e6fa9e
João Mozelli Neto <joaomozelli@hotmail.com>
2025-06-03 23:44:57 -0300

    implements environments variables

--- app/config/firebase.ts
+++ app/config/firebase.ts
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.ENV_FACEBOOK_API_KEY,
-  authDomain: process.env.ENV_FACEBOOK_AUTH_DOMAIN,
-  projectId: process.env.ENV_FACEBOOK_PROJECT_ID,
-  storageBucket: process.env.ENV_FACEBOOK_STORAGE_BUCKET,
-  messagingSenderId: process.env.ENV_FACEBOOK_MESSAGING_SENDER_ID,
-  appId: process.env.ENV_FACEBOOK_API_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_API_ID,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
